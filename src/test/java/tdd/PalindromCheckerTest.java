21749c6ac8665d5ed09dadc651429bce482d21e8
lars <lars.gentsch@gmail.com>
2019-11-07 21:16:12 +0100

    Added german test cases as comment

--- src/test/java/tdd/PalindromCheckerTest.java
+++ src/test/java/tdd/PalindromCheckerTest.java
@@ -1,5 +1,6 @@
 package tdd;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static tdd.PalindromChecker.isPalindrome;
 import org.junit.Test;
@@ -7,15 +8,28 @@ import org.junit.Test;
 public class PalindromCheckerTest {
 
     @Test
-    public void Return_true_for_a_valid_palindrom() {
+    public void A_text_with_valid_palindrome_should_work() {
         String text = "anna";
         boolean isPalindrome = isPalindrome("anna");
         assertTrue("Given text should be a palindrome.", isPalindrome );
     }
 
-    // Return false for a invalid palindrome
+    // Ein text der kein Palindrom ist soll abgelehnt werden
+    public void A_text_with_a_invalid_palidrome_should_be_rejected() {
+        String text = "bus";
+        boolean isPalindrome = isPalindrome(text);
+        assertFalse("Given text should be a palindrome.", isPalindrome );
+    }
 
-    // Return false for a value of NULL
+    // Test mit führende Leerzeichen ist kein Palindrom
 
-    // Return false for a empty string
+    // Ein null-Objekt ist kein Palindrom
+
+    // Groß- und Kleinschreibung soll ignoriert werden
+
+    // Sonderzeichen sind kein Palindrom
+
+    // Leerzeichen vor oder hinten sollen ignoriert werden
+
+
 }
