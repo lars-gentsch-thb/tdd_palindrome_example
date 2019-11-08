b1e5bef4d47934b023d205bcf416612903945fd3
lars <lars.gentsch@gmail.com>
2019-11-08 13:19:45 +0100

    Switched project to JUnit5.

--- src/test/java/tdd/PalindromCheckerTest.java
+++ src/test/java/tdd/PalindromCheckerTest.java
@@ -1,9 +1,11 @@
 package tdd;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static tdd.PalindromChecker.isPalindrome;
-import org.junit.Test;
+
 
 public class PalindromCheckerTest {
 
@@ -11,25 +13,22 @@ public class PalindromCheckerTest {
     public void A_text_with_valid_palindrome_should_work() {
         String text = "anna";
         boolean isPalindrome = isPalindrome("anna");
-        assertTrue("Given text should be a palindrome.", isPalindrome );
+        assertTrue(isPalindrome, "Given text should be a palindrome.");
     }
 
     // Ein text der kein Palindrom ist soll abgelehnt werden
     public void A_text_with_a_invalid_palidrome_should_be_rejected() {
         String text = "bus";
         boolean isPalindrome = isPalindrome(text);
-        assertFalse("Given text should be a palindrome.", isPalindrome );
+        assertFalse(isPalindrome, "Given text should not be a palindrome.");
     }
 
-    // Test mit führende Leerzeichen ist kein Palindrom
+    // Leerzeichen vor oder hinten sollen ignoriert werden
 
     // Ein null-Objekt ist kein Palindrom
 
     // Groß- und Kleinschreibung soll ignoriert werden
 
     // Sonderzeichen sind kein Palindrom
-
-    // Leerzeichen vor oder hinten sollen ignoriert werden
-
 
 }
